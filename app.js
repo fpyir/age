faa1025e0a5208d78cce976e8312026380ea92b1
Adam Short <adam010101@icloud.com>
2016-10-25 09:38:39 +1100

    Edited the JS code, and removed all the fat now that I'm sober.

--- app.js
+++ app.js
@@ -1,51 +1,26 @@
-var $input = $("input");
-var $status = $("img");
-var $age = $(".age");
-
 var rID;
-var sID;
- function createDate(date) {
-   var date = moment(date, "DD-MM-YYYY");
- }
 
- if (localStorage.birthday) {
-   console.log("");
-   sID = displayAge(localStorage.birthday);
- }
+if (localStorage.birthday) { displayAge(localStorage.birthday); }
 
-
- //31556952000 milliseconds in a year
- function calculateAge(birthDate) {
-   var currentDate = moment();
-  var elapsedTime = currentDate.diff(birthDate) / 31556952000;
-  return elapsedTime;
+function calculateAge(birthDate) {
+  return moment().diff(birthDate) / 31556952000; //31556952000 milliseconds in a year
 }
 
 function displayAge(input) {
-  var birthday;
   localStorage.setItem('birthday', input);
-  birthday = localStorage.birthday;
+  var birthday = localStorage.birthday;
+
   rID = setInterval(function() {
-    var age = calculateAge(birthday);
-    $age.text(age.toFixed(9));
+    var age = calculateAge(birthday).toFixed(9);
+    $(".age").text(age);
   }, 50);
 }
 
-$input.keyup(function () {
+$("input").keyup(function () {
   var input = $(this).val();
-  var strp = input.split("/");
-  if (strp == "undefined") { return }
-  if (strp.length == 3) {
-    if (strp[0].length == 2 && strp[1].length == 2 && strp[2].length == 4) {
-      days = parseInt(strp[0]);
-      months = parseInt(strp[1]);
-      years = parseInt(strp[2]);
-      if (!isNaN(days) && !isNaN(months) && !isNaN(years)) {
-        $status.attr("src", "yes.png");
-        clearInterval(rID);
-        clearInterval(sID);
-        displayAge(input);
-      }
-    }
-  }
+  if (/\d\d\/\d\d\/\d\d\d\d/.test(input)) { //regex match
+    $("img").attr("src", "yes.png");
+    clearInterval(rID);
+    displayAge(input);
+  } else {  $("img").attr("src", "no.png");  }
 });
